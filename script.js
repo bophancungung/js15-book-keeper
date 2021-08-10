c84d45b33c864a2b2a5a34a20c39a98a5ff6fe80
bophancungung <bophancungung@gmail.com>
2021-08-10 18:02:26 +0700

    Form validation

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', e => e.target === modal ? modal.classList.remove('show-modal') : false );
+
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert('Please sumbit values for both fields.')
+    return false;
+  }
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  // Valid
+  return true;
+}
+
+// Handle data from From
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  };
+  console.log(nameValue, urlValue);
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
